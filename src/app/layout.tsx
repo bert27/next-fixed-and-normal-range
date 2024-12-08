e420c3b1097bf134779b66c990f0b3aa1c843922
bert27 <zzopp@hotmail.com>
2024-12-08 17:30:00 +0100

    chore: update page title and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,20 @@
 // src/app/layout.tsx
 import StyledComponentsRegistry from "@/lib/registry";
 import "./globals.css";
+import { Metadata } from "next";
 
 interface RootLayoutProps {
   children: React.ReactNode;
 }
+
+export const metadata: Metadata = {
+  title: "Range Sliders – Fixed & Normal Modes",
+  description:
+    "Explore a powerful range slider demo with fixed and customizable modes, built using Next.js and styled for modern web applications.",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
